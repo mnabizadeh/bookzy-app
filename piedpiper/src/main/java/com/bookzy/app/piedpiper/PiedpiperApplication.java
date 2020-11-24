4d4c12fa5e22d9a28ef29f1d04303797e3d0fe60
student <mandana@cox.net>
2020-11-24 19:31:45 +0000

    add new files

---
+++ piedpiper/src/main/java/com/bookzy/app/piedpiper/PiedpiperApplication.java
@@ -0,0 +1,13 @@
+package com.bookzy.app.piedpiper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiedpiperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiedpiperApplication.class, args);
+	}
+
+}
---
+++ piedpiper/src/test/java/com/bookzy/app/piedpiper/PiedpiperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookzy.app.piedpiper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiedpiperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
